a6fa8829b8cad58d8ddbb3e452db9514d0f137eb
Alan Danque <adanque@gmail.com>
2025-10-14 11:34:04 -0500

    update startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 pip install poetry 
 poetry install --no-root 
-poetry run gunicorn --bind=0.0.0.0 --workers=2 --timeout=600 load
+poetry run load
+#poetry run gunicorn --bind=0.0.0.0 --workers=2 --timeout=600 load
 # poetry run gunicorn --bind=0.0.0.0 --workers=4 --timeout=600 my_app.wsgi:application
 # poetry run gunicorn --workers 2 --worker-class uvicorn.workers.UvicornWorker app.main:app --timeout 600
